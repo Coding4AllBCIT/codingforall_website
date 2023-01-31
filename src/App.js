0e6ccef4dbe0864576502e4240b2e293e56f9b54
ChillyChaos <josh123198@gmail.com>
2023-01-31 01:57:17 -0800

    Updated components to conform to React router
    Scrapped previous work and rewrote index.js and App.js to utilize React router and hooks. still needs work as there are errors in rendering due to invalid hook calls

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import Header from "./Components/Header";
-import Title from "./Components/Title";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 
-function App() {
+const App = () => {
   return (
-    <>
-    <Title></Title>
-    <Header></Header>
-    </>
+    <Router>
+      <div>
+        <h2>Welcome to React Router Tutorial</h2>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li>
+              <Link to={"/"} className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to={"/contact"} className="nav-link">
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link to={"/about"} className="nav-link">
+                About
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <hr />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Header() {
-  return <div>Header</div>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <header className="headerBackground">
+      <button
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+        aria-label="Toggle menu"
+      >
+        &#9776;
+      </button>
+      {isMenuOpen && (
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </header>
+  );
 }
 
 export default Header;
